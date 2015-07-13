0e6113960eb4bfd8e05a31b7e229ce2dd72abe48
hauspa <pasci@pascals-MacBook-Pro.local>
2015-07-13 12:29:56 +0200

    snippet for hero divider and social icons

---
+++ site/snippets/heroDivider.php
@@ -0,0 +1,11 @@
+<div class="mt2">
+  <a class="display-inline mr3">
+    <img src="../images/social/facebook.svg" width="30px">
+  </a>
+  <a class="display-inline mr3">
+    <img src="../images/social/twitter.svg" width="36px">
+  </a>
+  <a class="display-inline">
+    <img src="../images/social/instagram.svg" width="30px">
+  </a>
+</div>
