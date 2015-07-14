08c5707961d43b55327b637a5aabee0fc068a28f
Timothy Achumba <tim@6wunderkinder.com>
2015-07-14 09:30:00 +0200

    Refactored the events section

--- site/snippets/events.php
+++ site/snippets/events.php
@@ -1,74 +1,76 @@
 <!-- Typograph > Positioning > Layout > Color & Theme > State > Custom -->
 
-<div class="full-width border-box clearfix py4 px4">
-  <div class="container center">
+<div class="container py4 clearfix">
+  <div class="center">
     <h1 class="m0 caps lighter">This Week</h1>
-    <p class="h3 px1 mb3 col-inline col-8">As a local church we are a community of everyday people in which Jesus is central to all we do as we seek to embrace our city and nation.</p>
+    <p class="h3 px1 col-inline col-8">As a local church we are a community of everyday people in which Jesus is central to all we do as we seek to embrace our city and nation.</p>
   </div>
-  <div class="event-container inline-block border-box p2 mt2">
-    <div class="relative drop-shadow event bg-light-gray p1 cover-bg bottom-bg" <?php if($image = $page->image('3.jpg')): ?>style="background-image: url(<?= $image->url() ?>)"<?php endif ?>>
-      <div class="date align-left bc-white ml1">
-        <h4 class="caps heavy m0 mt2 line-height-0">MAY</h4>
-        <h2 class="caps bold m0">28</h2>
-      </div>
-      <div class="bg-faded-bc-white absolute full-width border-box bottom-0 left-0">
-        <div class="p2">
-          <h5 class="m0 caps mb1">Baptisms at the spree</h5>
-          <p class="light-gray m0">Come and celebrate with us as some of our family decides to make a public expression of their inward decision to follow Jesus.</p>
+  <div class="col col-12 py3">
+    <div class="col col-3 event-container inline-block border-box p1 mt2">
+      <div class="relative drop-shadow event bg-light-gray p1 width-bg top-bg" <?php if($image = $page->image('3.jpg')): ?>style="background-image: url(<?= $image->url() ?>)"<?php endif ?>>
+        <div class="date align-left bc-white ml1">
+          <h4 class="caps heavy m0 mt2 line-height-0">MAY</h4>
+          <h2 class="caps bold m0">28</h2>
         </div>
-        <div class="bg-bc-off-white px2 py1">
-          <img src="../images/ui/location.svg" width="8px" class="mr1"><p class="h6 bold inline-block caps m0">Schlachtensee</p>
+        <div class="bg-faded-bc-white absolute full-width border-box bottom-0 left-0">
+          <div class="p2">
+            <h5 class="m0 caps mb1">Baptisms at the spree</h5>
+            <p class="small light-gray m0">Come and celebrate with us as some of our family decides to make a public expression of their inward decision to follow Jesus.</p>
+          </div>
+          <div class="bg-bc-off-white px2 py1">
+            <img src="../images/ui/location.svg" width="8px" class="mr1"><h6 class="caps inline-block m0">Schlachtensee</h6>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  <div class="event-container inline-block border-box p2 mt2">
-    <div class="relative drop-shadow event bg-light-gray p1 cover-bg bottom-bg" <?php if($image = $page->image('33.jpg')): ?>style="background-image: url(<?= $image->url() ?>)"<?php endif ?>>
-      <div class="date align-left bc-white ml1">
-        <h4 class="caps heavy m0 mt2 line-height-0">MAY</h4>
-        <h2 class="caps bold m0">28</h2>
-      </div>
-      <div class="bg-faded-bc-white absolute full-width border-box bottom-0 left-0">
-        <div class="p2">
-          <h5 class="m0 caps mb1">Baptisms at the spree</h5>
-          <p class="light-gray m0">Come and celebrate with us as some of our family decides to make a public expression of their inward decision to follow Jesus.</p>
+    <div class="col col-3 event-container inline-block border-box p1 mt2">
+      <div class="relative drop-shadow event bg-light-gray p1 width-bg top-bg" <?php if($image = $page->image('33.jpg')): ?>style="background-image: url(<?= $image->url() ?>)"<?php endif ?>>
+        <div class="date align-left bc-white ml1">
+          <h4 class="caps heavy m0 mt2 line-height-0">MAY</h4>
+          <h2 class="caps bold m0">28</h2>
         </div>
-        <div class="bg-bc-off-white px2 py1">
-          <img src="../images/ui/location.svg" width="8px" class="mr1"><p class="h6 bold inline-block caps m0">Schlachtensee</p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="event-container inline-block border-box p2 mt2">
-    <div class="relative drop-shadow event bg-light-gray p1 cover-bg bottom-bg" <?php if($image = $page->image('1.jpg')): ?>style="background-image: url(<?= $image->url() ?>)"<?php endif ?>>
-      <div class="date align-left bc-white ml1">
-        <h4 class="caps heavy m0 mt2 line-height-0">MAY</h4>
-        <h2 class="caps bold m0">28</h2>
-      </div>
-      <div class="bg-faded-bc-white absolute full-width border-box bottom-0 left-0">
-        <div class="p2">
-          <h5 class="m0 caps mb1">Baptisms at the spree</h5>
-          <p class="light-gray m0">Come and celebrate with us as some of our family decides to make a public expression of their inward decision to follow Jesus.</p>
-        </div>
-        <div class="bg-bc-off-white px2 py1">
-          <img src="../images/ui/location.svg" width="8px" class="mr1"><p class="h6 bold inline-block caps m0">Schlachtensee</p>
+        <div class="bg-faded-bc-white absolute full-width border-box bottom-0 left-0">
+          <div class="p2">
+            <h5 class="m0 caps mb1">Baptisms at the spree</h5>
+            <p class="small light-gray m0">Come and celebrate with us as some of our family decides to make a public expression of their inward decision to follow Jesus.</p>
+          </div>
+          <div class="bg-bc-off-white px2 py1">
+            <img src="../images/ui/location.svg" width="8px" class="mr1"><h6 class="caps inline-block m0">Schlachtensee</h6>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  <div class="event-container inline-block border-box p2 mt2">
-    <div class="relative drop-shadow event bg-light-gray p1 cover-bg bottom-bg" <?php if($image = $page->image('2.jpg')): ?>style="background-image: url(<?= $image->url() ?>)"<?php endif ?>>
-      <div class="date align-left bc-white ml1">
-        <h4 class="caps heavy m0 mt2 line-height-0">MAY</h4>
-        <h2 class="caps bold m0">28</h2>
-      </div>
-      <div class="bg-faded-bc-white absolute full-width border-box bottom-0 left-0">
-        <div class="p2">
-          <h5 class="m0 caps mb1">Baptisms at the spree</h5>
-          <p class="light-gray m0">Come and celebrate with us as some of our family decides to make a public expression of their inward decision to follow Jesus.</p>
+    <div class="col col-3 event-container inline-block border-box p1 mt2">
+      <div class="relative drop-shadow event bg-light-gray p1 width-bg top-bg" <?php if($image = $page->image('1.jpg')): ?>style="background-image: url(<?= $image->url() ?>)"<?php endif ?>>
+        <div class="date align-left bc-white ml1">
+          <h4 class="caps heavy m0 mt2 line-height-0">MAY</h4>
+          <h2 class="caps bold m0">28</h2>
         </div>
-        <div class="bg-bc-off-white px2 py1">
-          <img src="../images/ui/location.svg" width="8px" class="mr1"><p class="h6 bold inline-block caps m0">Schlachtensee</p>
+        <div class="bg-faded-bc-white absolute full-width border-box bottom-0 left-0">
+          <div class="p2">
+            <h5 class="m0 caps mb1">Baptisms at the spree</h5>
+            <p class="small light-gray m0">Come and celebrate with us as some of our family decides to make a public expression of their inward decision to follow Jesus.</p>
+          </div>
+          <div class="bg-bc-off-white px2 py1">
+            <img src="../images/ui/location.svg" width="8px" class="mr1"><h6 class="caps inline-block m0">Schlachtensee</h6>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col col-3 event-container inline-block border-box p1 mt2">
+      <div class="relative drop-shadow event bg-light-gray p1 width-bg top-bg" <?php if($image = $page->image('2.jpg')): ?>style="background-image: url(<?= $image->url() ?>)"<?php endif ?>>
+        <div class="date align-left bc-white ml1">
+          <h4 class="caps heavy m0 mt2 line-height-0">MAY</h4>
+          <h2 class="caps bold m0">28</h2>
+        </div>
+        <div class="bg-faded-bc-white absolute full-width border-box bottom-0 left-0">
+          <div class="p2">
+            <h5 class="m0 caps mb1">Baptisms at the spree</h5>
+            <p class="small light-gray m0">Come and celebrate with us as some of our family decides to make a public expression of their inward decision to follow Jesus.</p>
+          </div>
+          <div class="bg-bc-off-white px2 py1">
+            <img src="../images/ui/location.svg" width="8px" class="mr1"><h6 class="caps inline-block m0">Schlachtensee</h6>
+          </div>
         </div>
       </div>
     </div>
