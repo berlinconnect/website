6407e5c24ef83d9ac4468f43a2c890719c8d6a26
Timothy Achumba <tim@6wunderkinder.com>
2015-07-14 10:31:52 +0200

    Built the footer section

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,30 +1,16 @@
-<!-- <div class="full-width px2 py4 bg-dark-gray light-gray border-box">
-  <div class="container">
-    <img class="inline-block mr4 muted" src="../images/logo/light-white.svg" width="100px"/>
-    <div class="right">
-      <ul class="list-reset bold m0 inline-block mr4 vt">
-        <li class="caps h5 m0 mb1">Home</li>
-        <li class="caps h5 m0 mb1">CALENDAR</li>
-        <li class="caps h5 m0 mb1">CONFERENCES</li>
-        <li class="caps h5 m0 mb1">KIDS</li>
-        <li class="caps h5 m0 mb1">YOUTH</li>
-      </ul>
-      <ul class="list-reset bold m0 inline-block mr4 vt">
-        <li class="caps h5 m0 mb1">Dinner parties</li>
-        <li class="caps h5 m0 mb1">sisterhood</li>
-        <li class="caps h5 m0 mb1">Charity water</li>
-        <li class="caps h5 m0 mb1">futureminded</li>
-        <li class="caps h5 m0 mb1">giving</li>
-      </ul>
-      <ul class="list-reset bold m0 inline-block mr4 vt">
-        <li class="caps h5 m0 mb1">one team</li>
-        <li class="caps h5 m0 mb1">contact</li>
-        <li class="caps h5 m0 mb1">enlarge</li>
-        <li class="caps h5 m0 mb1">imprint</li>
-        <li class="caps h5 m0 mb1">our venue</li>
-      </ul>
+<div class="full-width py3 px3 bg-dark-gray light-gray border-box">
+  <div class="clearfix">
+    <div class="font-size-0">
+      <div class="col-inline col-8">
+        <img class="inline-block mr2 muted vm" src="../images/logo/light-white.svg" width="60px"/><p class="small inline-block vm m0"><?= $site->copyright() ?></p>
+      </div>
+      <div class="col-inline col-4 vm">
+        <div class="right muted">
+          <?php snippet('heroDivider') ?>
+        </div>
+      </div>
     </div>
   </div>
 </div>
 </body>
-</html> -->
+</html>
