cbeb57159429ec1632f94da4b2c96d2533f9c0f7
Timothy Achumba <tim@6wunderkinder.com>
2015-07-16 20:40:36 +0200

    Improved the footer and built the new page

--- site/snippets/grid.php
+++ site/snippets/grid.php
@@ -9,7 +9,7 @@
       <div class="relative mx-auto grid-content">
         <h5 class="caps m0 mb3 bc-blue"><?= $page->abouttitle() ?></h5>
         <p class="h4 bold caps line-height-2 m0 mb2"><?= $page->aboutheader() ?></p>
-        <p class="h4"><?= $page->abouttext() ?></p>
+        <p class="h4 regular"><?= $page->abouttext() ?></p>
       </div>
     </div>
   </div>
@@ -22,7 +22,7 @@
       <div class="relative mx-auto grid-content">
         <h5 class="caps m0 mb3 bc-orange">ENLARGE</h5>
         <p class="h4 bold caps line-height-2 m0 mb2">This is the title of the message of the month</p>
-        <p class="h4">This sunday we go to hear an amazing message from Mark. He spoke about something really awesome....</p>
+        <p class="h4 regular">This sunday we go to hear an amazing message from Mark. He spoke about something really awesome....</p>
         <a href="#" class="h5 bold caps m0 mt3 full-width inline-block bg-bc-white bc-orange">Listen Now</a>
       </div>
     </div>
@@ -36,7 +36,7 @@
       <div class="relative mx-auto grid-content">
         <h5 class="caps m0 mb3 light-gray"><?= $page->subscribetitle() ?></h5>
         <p class="h4 bold caps line-height-2 m0 mb1"><?= $page->subscribeheader() ?></p>
-        <p class="h4 m0 mb2"><?= $page->subscribetext() ?></p>
+        <p class="h4 regular m0 mb2"><?= $page->subscribetext() ?></p>
         <input class="p2 mt2 full-width border-box bg-bc-off-white" placeholder="<?= $page->subscribeinputplaceholder() ?>"></input>
         <button type="submit" class="p2 center full-width border-none bg-dark-gray bc-white"><?= $page->subscribesubmit() ?></button>
       </div>
---
+++ site/templates/new.php
@@ -0,0 +1,31 @@
+<?php snippet('header') ?>
+<?php snippet('hero') ?>
+<div class="container pt4 mt3 mb3 clearfix">
+  <div class="col col-5">
+    <p class="h3 bc-blue lighter">Our philosophy is simply to love God, love people and love life. </p>
+  </div>
+  <div class="col-right col-6">
+    <p>We are multicultural church in the heart of Berlin, changing people's perceptions about Jesus and His Church. Our philosophy is simply to love God, love people and love life. Our motto is - 'Come as you are'</p>
+  </div>
+</div>
+<div class="full-width border-box p3 clearfix">
+  <div class="col col-12">
+    <div class="font-size-0">
+      <div class="col-inline col-6 vt border-box p1">
+        <?php if($image = $page->image('photo-1.jpg')): ?><img src="<?= $image->url() ?>" width="100%" height="100%" /><?php endif ?>
+      </div>
+      <div class="col-inline col-6 vt">
+        <div class="col-inline col-4 vt border-box p1"><?php if($image = $page->image('photo-1.jpg')): ?><img src="<?= $image->url() ?>" width="100%" /><?php endif ?></div>
+        <div class="col-inline col-4 vt border-box p1"><?php if($image = $page->image('photo-1.jpg')): ?><img src="<?= $image->url() ?>" width="100%" /><?php endif ?></div>
+        <div class="col-inline col-4 vt border-box p1"><?php if($image = $page->image('photo-1.jpg')): ?><img src="<?= $image->url() ?>" width="100%" /><?php endif ?></div>
+        <div class="col-inline col-4 vt border-box p1"><?php if($image = $page->image('photo-1.jpg')): ?><img src="<?= $image->url() ?>" width="100%" /><?php endif ?></div>
+        <div class="col-inline col-4 vt border-box p1"><?php if($image = $page->image('photo-1.jpg')): ?><img src="<?= $image->url() ?>" width="100%" /><?php endif ?></div>
+        <div class="col-inline col-4 vt border-box p1"><?php if($image = $page->image('photo-1.jpg')): ?><img src="<?= $image->url() ?>" width="100%" /><?php endif ?></div>
+        <div class="col-inline col-4 vt border-box p1"><?php if($image = $page->image('photo-1.jpg')): ?><img src="<?= $image->url() ?>" width="100%" /><?php endif ?></div>
+        <div class="col-inline col-4 vt border-box p1"><?php if($image = $page->image('photo-1.jpg')): ?><img src="<?= $image->url() ?>" width="100%" /><?php endif ?></div>
+        <div class="col-inline col-4 vt border-box p1"><?php if($image = $page->image('photo-1.jpg')): ?><img src="<?= $image->url() ?>" width="100%" /><?php endif ?></div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php snippet('footer') ?>
