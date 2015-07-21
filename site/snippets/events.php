44a56531e939b91a50578ee0118e0a6ff7015806
hauspa <pasci@pascals-MacBook-Pro.local>
2015-07-21 12:00:39 +0200

    limit to 4 events posted
    also put the button at the end in new container, to place it in new row.

--- site/snippets/events.php
+++ site/snippets/events.php
@@ -1,19 +1,41 @@
 <!-- Typograph > Positioning > Layout > Color & Theme > State > Custom -->
 
-<div class="container py4 clearfix">
+<div class="container pt4 clearfix">
   <div class="center">
     <h1 class="m0 caps lighter"><?= $site->eventsectiontitle() ?></h1>
     <p class="h3 regular px1 col-inline col-8"><?= $site->eventsectioninfo() ?></p>
   </div>
   <div class="questions grid">
     <?php 
+      /* 
+        since the built-in Kirby limit() function won't be useable, 
+        because we need to filter dates as well,
+        use $limitEvents variable to keep track of how many events have 
+        already been posted
+      */
+      $limitEvents = 1;
+      // get the current date and the date in seven days
       $now = date('M d, Y');
       $now = strtotime($now);
       $sevenDays = strtotime("+7 day", $now); 
     ?>
     <?php foreach($pages->find('calendar')->children()->visible() as $event): ?>
-      <?php $date = $event->date(); ?>
-      <?php if ($date <= $sevenDays and $now <= $date ): ?>
+      <?php 
+        // get date of the event
+        $date = $event->date(); 
+      ?>
+      <?php 
+        /* 
+          only show the event if the event is not obsolete
+          and will happen in less than or in exactly 7 days
+          and there are less than 4 events posted already 
+        */
+        if ($date <= $sevenDays and $now <= $date and $limitEvents <= 4): 
+      ?>
+        <?php 
+          // increase by one, as part of keeping track how many events were posted already
+          $limitEvents = $limitEvents + 1; 
+        ?>
         <div class="col col-3 event-container inline-block border-box p1 mt2">
         <!-- IMPORTANT: the image in the content folder/for every event must be named 1.jpg!!!!!!! -->
           <div class="relative drop-shadow event bg-light-gray p1 width-bg top-bg" <?php if($image = $event->image('1.jpg')): ?>style="background-image: url(<?= $image->url() ?>)"<?php endif ?>>
@@ -35,6 +57,8 @@
       <?php endif ?>
     <?php endforeach ?>
   </div>
+</div>
+<div class="container pb4 clearfix">
   <div class="center mt3">
     <a class="button bg-bc-blue py2 px4">See full calendar</a>
   </div>
