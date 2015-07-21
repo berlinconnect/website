ebdd141e3a9237e97452424310aceebf8b1a1f46
hauspa <pasci@pascals-MacBook-Pro.local>
2015-07-21 11:35:21 +0200

    filter for next seven days

--- site/snippets/events.php
+++ site/snippets/events.php
@@ -2,78 +2,38 @@
 
 <div class="container py4 clearfix">
   <div class="center">
-    <h1 class="m0 caps lighter">This Week</h1>
-    <p class="h3 regular px1 col-inline col-8">As a local church we are a community of everyday people in which Jesus is central to all we do as we seek to embrace our city and nation.</p>
+    <h1 class="m0 caps lighter"><?= $site->eventsectiontitle() ?></h1>
+    <p class="h3 regular px1 col-inline col-8"><?= $site->eventsectioninfo() ?></p>
   </div>
-  <div class="col col-12 py3">
-    <div class="col col-3 event-container inline-block border-box p1 mt2">
-      <div class="relative drop-shadow event bg-light-gray p1 width-bg top-bg" <?php if($image = $page->image('3.jpg')): ?>style="background-image: url(<?= $image->url() ?>)"<?php endif ?>>
-        <div class="date align-left bc-white ml1">
-          <h4 class="caps heavy m0 mt2 line-height-0">MAY</h4>
-          <h2 class="caps bold m0">28</h2>
-        </div>
-        <div class="bg-faded-bc-white absolute full-width border-box bottom-0 left-0">
-          <div class="p2">
-            <h5 class="m0 caps mb1">Baptisms at the spree</h5>
-            <p class="small light-gray m0">Come and celebrate with us as some of our family decides to make a public expression of their inward decision to follow Jesus.</p>
-          </div>
-          <div class="bg-bc-off-white px2 py1">
-            <img src="../images/ui/location.svg" width="8px" class="mr1"><h6 class="caps inline-block m0">Schlachtensee</h6>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col col-3 event-container inline-block border-box p1 mt2">
-      <div class="relative drop-shadow event bg-light-gray p1 width-bg top-bg" <?php if($image = $page->image('33.jpg')): ?>style="background-image: url(<?= $image->url() ?>)"<?php endif ?>>
-        <div class="date align-left bc-white ml1">
-          <h4 class="caps heavy m0 mt2 line-height-0">MAY</h4>
-          <h2 class="caps bold m0">28</h2>
-        </div>
-        <div class="bg-faded-bc-white absolute full-width border-box bottom-0 left-0">
-          <div class="p2">
-            <h5 class="m0 caps mb1">Baptisms at the spree</h5>
-            <p class="small light-gray m0">Come and celebrate with us as some of our family decides to make a public expression of their inward decision to follow Jesus.</p>
-          </div>
-          <div class="bg-bc-off-white px2 py1">
-            <img src="../images/ui/location.svg" width="8px" class="mr1"><h6 class="caps inline-block m0">Schlachtensee</h6>
+  <div class="questions grid">
+    <?php 
+      $now = date('M d, Y');
+      $now = strtotime($now);
+      $sevenDays = strtotime("+7 day", $now); 
+    ?>
+    <?php foreach($pages->find('calendar')->children()->visible() as $event): ?>
+      <?php $date = $event->date(); ?>
+      <?php if ($date <= $sevenDays and $now <= $date ): ?>
+        <div class="col col-3 event-container inline-block border-box p1 mt2">
+        <!-- IMPORTANT: the image in the content folder/for every event must be named 1.jpg!!!!!!! -->
+          <div class="relative drop-shadow event bg-light-gray p1 width-bg top-bg" <?php if($image = $event->image('1.jpg')): ?>style="background-image: url(<?= $image->url() ?>)"<?php endif ?>>
+            <div class="date align-left bc-white ml1">
+              <h4 class="caps heavy m0 mt2 line-height-0"><?php echo $event->date('M') ?></h4>
+              <h2 class="caps bold m0"><?php echo $event->date('d') ?></h2>
+            </div>
+            <div class="bg-faded-bc-white absolute full-width border-box bottom-0 left-0">
+              <div class="p2">
+                <h5 class="m0 caps mb1"><?php echo $event->title() ?></h5>
+                <p class="small light-gray m0"><?php echo $event->text() ?></p>
+              </div>
+              <div class="bg-bc-off-white px2 py1">
+                <a class='dark-gray' href='<?php echo $event->googlemapslink() ?>'><img src="../images/ui/location.svg" width="8px" class="mr1"><h6 class="caps inline-block m0"><?php echo $event->location() ?></h6></a>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-    <div class="col col-3 event-container inline-block border-box p1 mt2">
-      <div class="relative drop-shadow event bg-light-gray p1 width-bg top-bg" <?php if($image = $page->image('1.jpg')): ?>style="background-image: url(<?= $image->url() ?>)"<?php endif ?>>
-        <div class="date align-left bc-white ml1">
-          <h4 class="caps heavy m0 mt2 line-height-0">MAY</h4>
-          <h2 class="caps bold m0">28</h2>
-        </div>
-        <div class="bg-faded-bc-white absolute full-width border-box bottom-0 left-0">
-          <div class="p2">
-            <h5 class="m0 caps mb1">Baptisms at the spree</h5>
-            <p class="small light-gray m0">Come and celebrate with us as some of our family decides to make a public expression of their inward decision to follow Jesus.</p>
-          </div>
-          <div class="bg-bc-off-white px2 py1">
-            <img src="../images/ui/location.svg" width="8px" class="mr1"><h6 class="caps inline-block m0">Schlachtensee</h6>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col col-3 event-container inline-block border-box p1 mt2">
-      <div class="relative drop-shadow event bg-light-gray p1 width-bg top-bg" <?php if($image = $page->image('2.jpg')): ?>style="background-image: url(<?= $image->url() ?>)"<?php endif ?>>
-        <div class="date align-left bc-white ml1">
-          <h4 class="caps heavy m0 mt2 line-height-0">MAY</h4>
-          <h2 class="caps bold m0">28</h2>
-        </div>
-        <div class="bg-faded-bc-white absolute full-width border-box bottom-0 left-0">
-          <div class="p2">
-            <h5 class="m0 caps mb1">Baptisms at the spree</h5>
-            <p class="small light-gray m0">Come and celebrate with us as some of our family decides to make a public expression of their inward decision to follow Jesus.</p>
-          </div>
-          <div class="bg-bc-off-white px2 py1">
-            <img src="../images/ui/location.svg" width="8px" class="mr1"><h6 class="caps inline-block m0">Schlachtensee</h6>
-          </div>
-        </div>
-      </div>
-    </div>
+      <?php endif ?>
+    <?php endforeach ?>
   </div>
   <div class="center mt3">
     <a class="button bg-bc-blue py2 px4">See full calendar</a>
