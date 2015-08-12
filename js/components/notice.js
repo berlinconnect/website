d1fb32fbb984c4aaa60f552895d984092c6d7b1b
Timothy Achumba <tim@6wunderkinder.com>
2015-08-12 18:49:33 +0200

    More notice improvements

--- js/components/notice.js
+++ js/components/notice.js
@@ -1,12 +1,29 @@
 var $toggleNotice = $('.notice .header');
+var $closeNotice = $('.close-notice');
 var $overlay = $('.overlay');
 var $notice = $('.notice');
 
+function fixedBody() {
+  $('body').toggleClass('overflow-hidden');
+}
+
 function toggleNotice() {
-  $overlay.toggleClass('fadedVisible');
+  $overlay.toggleClass('zoomed');
   $notice.toggleClass('minified');
 }
 
+function openNotice() {
+  $overlay.addClass('zoomed');
+  $notice.removeClass('minified');
+}
+
+
 $toggleNotice.click(function() {
+  openNotice();
+  fixedBody();
+});
+
+$closeNotice.click(function() {
   toggleNotice();
+  fixedBody();
 });
--- site/snippets/notice.php
+++ site/snippets/notice.php
@@ -1,8 +1,11 @@
+<div class="overlay">
+
+</div>
 <div class="fixed bg-bc-white z4 heavy-drop-shadow animated box-sizing notice minified">
-  <div class="heavy-drop-shadow">
+  <div class="clearfix heavy-drop-shadow">
     <div class="relative header bg-bc-blue bc-white overflow-hidden pointer">
       <div class="heading clearfix p2">
-        <h6 class="m0 col col-10 inline-block vm"><span class="h6 phalf r2 mr1 line-height-0 bg-bc-white bc-blue">New</span></span>Heart for the house 2015</h6>
+        <h6 class="m0 col col-10 inline-block vm"><span class="h6 phalf mr1 line-height-0 bg-bc-white bc-blue">New</span>Heart for the house</h6>
         <h6 class="m0 col col-2 regular right-align inline-block vm">Read</h6>
       </div>
       <div class="absolute full-height full-width cover-bg top-bg header-image z1" style="background-image:url('<?php echo $pages->find('notice')->images()->first()->url(); ?>');"></div>
@@ -13,6 +16,7 @@
         Our annual Heart for the House will be coming up very soon. To find out more about what it is and what it means for you, click the button below.
       </p>
     </div>
-    <a class="bg-bc-blue center bc-white block p2 caps h5">Visit</a>
+    <a class="col col-6 bg-bc-off-white center light-gray block p2 caps h5 close-notice">Dismiss</a>
+    <a class="col col-6 bg-bc-blue center bc-white block p2 caps h5">Visit</a>
   </div>
 </div>
