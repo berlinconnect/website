a955fb0e09ff038e99dbc563a7652190b7eb1bb7
hauspa <pasci@pascals-MacBook-Pro.local>
2015-08-27 10:46:55 +0200

    changed heroDivider to social snippet
    the previous heroDivider snippet was outdated, still not working though

--- site/templates/error.php
+++ site/templates/error.php
@@ -4,12 +4,8 @@
 		<div class="logo"></div>
 		<h1><?= html($page->title()) ?></h1>
 		<?= kirbytext($page->text()) ?>
-		<?php snippet('heroDivider') ?>
+		<?= snippet('social', array('iconSize' => '30px')) ?>
 	</div>
 </div>
 
-<?php echo js('js/libs/jquery.js') ?>
-<?php echo js('js/components/sidebar.js') ?>
-
-</body>
-</html>
+<?php snippet('footer') ?>
