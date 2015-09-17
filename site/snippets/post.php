7e94ff98aceebe4dbd464e1e07db32ed8ff5c6c2
hauspa <pasci@pascals-MacBook-Pro.local>
2015-09-17 16:58:31 +0200

    Added Conditional to showing post image

--- site/snippets/post.php
+++ site/snippets/post.php
@@ -15,7 +15,16 @@
 
   <span class="h6 bold caps absolute left-0 top-0 mt2 ml2 bc-white px1 py2 line-height-0 bg-bc-<?= $post->color() ?> z4"><?= $post->category() ?></span>
 
-  <div class="absolute full-height left-0 top-0 full-width cover-bg center-bg bg-image" style="background-image: url(<?= $post->images()->first()->url(); ?>)"></div>
+  <div class="absolute full-height left-0 top-0 full-width cover-bg center-bg bg-image" style="background-image: url(
+  <?php 
+  if ($post->hasImages()){
+    echo $post->images()->first()->url();
+  }
+  else{
+    echo $page->image('default_post.jpg')->url();
+  } 
+  ?>
+  )"></div>
 
   <div class="absolute full-height left-0 top-0 full-width  cover-bg bg-overlay-dark-gray"></div>
 
