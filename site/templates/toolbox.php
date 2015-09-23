434c9e441746debaddba6eda5ba0d641e2b015b7
Timothy ッ <timothy@achumba.com>
2015-09-23 09:39:14 +0200

    more fixes to the toolbox page

--- site/templates/toolbox.php
+++ site/templates/toolbox.php
@@ -8,7 +8,7 @@
       <?php
         $heroPost = $page->children()->flip()->visible()->limit(1)->first();
         $slug = $heroPost->slug();
-        foreach($page->children()->visible()->not($slug) as $post):
+        foreach($page->children()->flip()->visible()->not($slug) as $post):
       ?>
         <?php snippet('post', array('post' => $post)) ?>
       <?php endforeach ?>
