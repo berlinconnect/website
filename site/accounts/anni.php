dac31067b04d653dd596b8810993db526033021d
hauspa <pasci@pascals-MacBook-Pro.local>
2015-10-02 10:32:33 +0200

    new users

---
+++ site/accounts/anni.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: anni
+firstname: Anni
+lastname: Conrath-Hargreaves
+email: conrath.annemarie@gmail.com
+password: >
+  $2a$10$fYFIe4zlySm2xaSqU4OiV.UvGDbg7zl8dcuCnEU7gAZ1OYTDeP9rO
+language: en
+role: editor
