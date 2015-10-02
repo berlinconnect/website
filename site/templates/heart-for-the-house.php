06fc70342aeec837b6535ea17c5ef57a284f5496
hauspa <pasci@pascals-MacBook-Pro.local>
2015-10-02 09:19:06 +0200

    h4h: dynamic content
    also changed permission to be able to edit etc.

--- site/templates/heart-for-the-house.php
+++ site/templates/heart-for-the-house.php
@@ -4,23 +4,21 @@
 <div class="full-width clearfix">
   <div class="p4 bg-dark-gray">
     <div class="center video-wrapper border-box">
-      <iframe src="https://player.vimeo.com/video/139208268?badge=0" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+      <iframe src="<?= $page->video() ?>" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
     </div>
   </div>
 
     <div class="flex flex-stretch flex-column md-flex-row">
 
       <div class="flex flex-center col-12 md-col-6 full-height cover-bg flex-content-stretch center-bg h4h-image" style="background-image:url('<?= $pages->find('h4h2015')->image('giver1.jpg')->url(); ?>');">
-        <h1 class="flex-auto center bc-white bold letter-spacing-2 m0 py4">Faithful<br>Giver</h1>
+        <h1 class="flex-auto center bc-white bold letter-spacing-2 m0 py4"><?= $page->faithfulGiverTitle() ?></h1>
       </div>
 
       <div class="col-12 md-col-6 tiny-center sm-center md-left border-box full-height p2 xsm-p3 lg-p4">
 
-        <p class="h4 regular bc-blue caps line-height-2 m0 mb2">Bring a gift on the day</p>
+        <p class="h4 regular bc-blue caps line-height-2 m0 mb2"><?= $page->faithfulGiverHeader() ?></p>
 
-        <p class="tiny-justify">Faithful Givers are people who are willing and generous in their outlook on life and understand that their contribution large or small will always give God an opportunity to show His faithfulness in our lives.</p>
-
-        <p class="tiny-justify">Let’s prepare our hearts with prayer and excitement as we consider what to bring to the Heart for the House offering on 4th October 2015 enabling us to strengthen our church and speed up the work of Jesus here in our city Berlin.</p>
+        <p class="tiny-justify"><?= $page->faithfulGiverText() ?></p>
 
         <a href="" class="h5 caps bold p2 px3 center bg-bc-blue bc-white block" onclick="_gaq.push(['_trackEvent', 'Buttons', 'Clicked', 'Sign up for team']);">Online Giving</a>
 
@@ -41,14 +39,14 @@
     <div class="flex flex-stretch flex-column md-flex-row">
 
       <div class="flex flex-center col-12 md-col-6 full-height cover-bg flex-content-stretch center-bg h4h-image" style="background-image:url('<?= $pages->find('h4h2015')->image('giver2.jpg')->url(); ?>');">
-        <h1 class="flex-auto center bc-white bold letter-spacing-2 m0 py4">Vision<br>Giver</h1>
+        <h1 class="flex-auto center bc-white bold letter-spacing-2 m0 py4"><?= $page->visionGiverTitle() ?></h1>
       </div>
 
       <div class="flex-first col-12 md-col-6 tiny-center sm-center md-left border-box full-height p2 xsm-p3 lg-p4">
 
-        <p class="h4 regular bc-blue caps line-height-2 m0 mb2">Bring a gift on the day + a monthly commitment</p>
+        <p class="h4 regular bc-blue caps line-height-2 m0 mb2"><?= $page->visionGiverHeader() ?></p>
 
-        <p class="tiny-justify">Vision Givers bring a gift on 4th October plus a monthly commitment for 12 months resourcing the ongoing vision of Berlin Connect. This monthly commitment is above your normal giving of tithes and offerings. Vision giving starts at € 10 or more and runs for 12 months.</p>
+        <p class="tiny-justify"><?= $page->visionGiverText() ?></p>
 
         <a href="" class="h5 caps bold p2 px3 center bg-bc-blue bc-white block" onclick="_gaq.push(['_trackEvent', 'Buttons', 'Clicked', 'Sign up for team']);">Online Giving</a>
 
@@ -68,16 +66,14 @@
     <div class="flex flex-stretch flex-column md-flex-row">
 
       <div class="flex flex-center col-12 md-col-6 full-height cover-bg flex-content-stretch center-bg h4h-image" style="background-image:url('<?= $pages->find('h4h2015')->image('giver3.jpg')->url(); ?>');">
-        <h1 class="flex-auto center bc-white bold letter-spacing-2 m0 py4">Kingdom<br>Giver</h1>
+        <h1 class="flex-auto center bc-white bold letter-spacing-2 m0 py4"><?= $page->kingdomGiverTitle() ?></h1>
       </div>
 
       <div class="col-12 md-col-6 tiny-center sm-center md-left border-box full-height p2 xsm-p3 lg-p4">
 
-        <p class="h4 regular bc-blue caps line-height-2 m0 mb2">Bring a gift on the day + a yearly commitment</p>
+        <p class="h4 regular bc-blue caps line-height-2 m0 mb2"><?= $page->kingdomGiverHeader() ?></p>
 
-        <p class="tiny-justify">Kingdom Givers understand that their gifts, talents and character add value that strengthens the House of God. This is a vehicle to help people who believe they are called to be financial leaders in life and who choose to honour the Lord with their increase and wealth.</p>
-
-        <p class="tiny-justify">Kingdom giving starts at € 3,000.00 or more and is an annual commitment, over and above your normal tithes and offerings, to resource the ongoing work of Berlin Connect.</p>
+        <p class="tiny-justify"><?= $page->kingdomGiverText() ?></p>
 
         <a href="" class="h5 caps bold p2 px3 center bg-bc-blue bc-white block" onclick="_gaq.push(['_trackEvent', 'Buttons', 'Clicked', 'Sign up for team']);">Online Giving</a>
 
