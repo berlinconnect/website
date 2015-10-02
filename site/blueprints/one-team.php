49ad838a7f92e287caf90bf550ca8ac797d473f1
hauspa <pasci@pascals-MacBook-Pro.local>
2015-10-02 09:31:51 +0200

    one team blueprint

--- site/blueprints/one-team.php
+++ site/blueprints/one-team.php
@@ -7,3 +7,84 @@ fields:
   title:
     label: Title
     type:  title
+  introHeader:
+  	label: Intro Header
+  	type: textarea
+  introText:
+  	label: Intro Text
+  	type: textarea
+  TenReasonsHeader:
+  	label: Ten Reasons Header
+  	type: textarea
+  Reason1Header:
+  	label: Reason 1 Header
+  	type: textarea
+  Reason1Text:
+  	label: Reason 1 Text
+  	type: textarea
+  Reason2Header:
+  	label: Reason 2 Header
+  	type: textarea
+  Reason2Text:
+  	label: Reason 2 Text
+  	type: textarea
+  Reason3Header:
+  	label: Reason 3 Header
+  	type: textarea
+  Reason3Text:
+  	label: Reason 3 Text
+  	type: textarea
+  Reason4Header:
+  	label: Reason 4 Header
+  	type: textarea
+  Reason4Text:
+  	label: Reason 4 Text
+  	type: textarea
+  Reason5Header:
+  	label: Reason 5 Header
+  	type: textarea
+  Reason5Text:
+  	label: Reason 5 Text
+  	type: textarea
+  Reason6Header:
+  	label: Reason 6 Header
+  	type: textarea
+  Reason6Text:
+  	label: Reason 6 Text
+  	type: textarea
+  Reason7Header:
+  	label: Reason 7 Header
+  	type: textarea
+  Reason7Text:
+  	label: Reason 7 Text
+  	type: textarea
+  Reason8Header:
+  	label: Reason 8 Header
+  	type: textarea
+  Reason8Text:
+  	label: Reason 8 Text
+  	type: textarea
+  Reason9Header:
+  	label: Reason 9 Header
+  	type: textarea
+  Reason9Text:
+  	label: Reason 9 Text
+  	type: textarea
+  Reason10Header:
+  	label: Reason 10 Header
+  	type: textarea
+  Reason10Text:
+  	label: Reason 10 Text
+  	type: textarea
+  SignupHeader:
+  	label: Signup Header
+  	type: text
+  SignupText:
+  	label: Signup Text
+  	type: textarea
+  SignupCallToAction:
+  	label: Signup Call To Action
+  	type: text
+  MoreInstagram:
+  	label: More Instagram Stories
+  	type: text
