1adcdf121d736fc4db93e794f24dd32e5c8f3861
hauspa <pasci@pascals-MacBook-Pro.local>
2015-10-03 23:37:34 +0200

    snippet for each venue address

---
+++ site/snippets/gmap_hausungarn.php
@@ -0,0 +1,26 @@
+<!-- Maps -->
+  <script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+  <script>
+    function initialize() {
+      var myLatLng = new google.maps.LatLng(52.521300, 13.406281);
+      var map_canvas = document.getElementById('map_canvas');
+      var myIcon = new google.maps.MarkerImage("<?php echo url('images/logo/logo-black.png') ?>", null, null, null, new google.maps.Size(60,60));
+      var isDraggable = $(document).width() > 480 ? true : false;
+      var map_options = {
+        center: new google.maps.LatLng(52.521874, 13.410211),
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        scrollwheel: false,
+        draggable: isDraggable
+      }
+      var map = new google.maps.Map(map_canvas, map_options)
+      var marker = new google.maps.Marker({
+        position: myLatLng,
+        icon: myIcon,
+        map: map
+      });
+      map.panTo(marker.getPosition());
+      marker.setMap(map);
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+  </script>
