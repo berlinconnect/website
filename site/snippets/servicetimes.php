ccf22dc1bcfe9e4edb499da3194f7cbdc1d8336f
pascal hausberger <pasci@pascals-MacBook-Pro.local>
2015-12-01 14:39:10 +0100

    change of service times for this sunday
    commented out

--- site/snippets/servicetimes.php
+++ site/snippets/servicetimes.php
@@ -5,10 +5,10 @@
     <h4 class="bold mt3 mb2 tiny-mb2"><?= $page->servicetimes() ?></h4>
     <p class="h4 bold px1 sm-px2 col-inline col-12 md-col-8 bc-white">
       <span class="px1 border border-bc-white rounded mxn1 mb2 sm-mb0 inline-block">11:00</span>
-      <span class="px1 border border-bc-white rounded mxn1 mb2 sm-mb0 inline-block">13:00</span>
-      <span class="px1 border border-bc-white rounded mxn1 mb2 sm-mb0 inline-block">15:00</span>
+      <!-- <span class="px1 border border-bc-white rounded mxn1 mb2 sm-mb0 inline-block">13:00</span> -->
+      <!-- <span class="px1 border border-bc-white rounded mxn1 mb2 sm-mb0 inline-block">15:00</span> -->
       <span class="px1 border border-bc-white rounded mxn1 mb2 sm-mb0 inline-block">17:00</span>
-      <span class="px1 border border-bc-white rounded mxn1 mb2 sm-mb0 inline-block">19:00</span>
+      <!-- <span class="px1 border border-bc-white rounded mxn1 mb2 sm-mb0 inline-block">19:00</span> -->
     </p>
   </div>
   <div class="absolute left-0 top-0 full-height full-width bg-light-overlay-dark-gray no-flex"></div>
