ebc8991deebc14ab7c8eafe00b7328247533ee9d
pascal hausberger <pasci@pascals-MacBook-Pro.local>
2015-12-14 21:57:23 +0100

    hide installed for dinner parties

--- site/snippets/servicetimes.php
+++ site/snippets/servicetimes.php
@@ -4,9 +4,9 @@
     <p class="h3 lighter px1 sm-px2 col-inline col-12 md-col-8"><?= $page->oursundaygatheringstext() ?></p>
     <h4 class="bold mt3 mb2 tiny-mb2"><?= $page->servicetimes() ?></h4>
     <p class="h4 bold px1 sm-px2 col-inline col-12 md-col-8 bc-white">
-      <!-- <span class="px1 border border-bc-white rounded mxn1 mb2 sm-mb0 inline-block">11:00</span> -->
-      <!-- <span class="px1 border border-bc-white rounded mxn1 mb2 sm-mb0 inline-block">13:00</span> -->
-      <!-- <span class="px1 border border-bc-white rounded mxn1 mb2 sm-mb0 inline-block">15:00</span> -->
+      <span class="px1 border border-bc-white rounded mxn1 mb2 sm-mb0 inline-block">11:00</span>
+      <span class="px1 border border-bc-white rounded mxn1 mb2 sm-mb0 inline-block">13:00</span>
+      <span class="px1 border border-bc-white rounded mxn1 mb2 sm-mb0 inline-block">15:00</span>
       <span class="px1 border border-bc-white rounded mxn1 mb2 sm-mb0 inline-block">17:00</span>
       <span class="px1 border border-bc-white rounded mxn1 mb2 sm-mb0 inline-block">19:00</span>
     </p>
