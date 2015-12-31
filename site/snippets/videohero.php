b8664a810099e7b52c43e081f27add70f217305b
pascal hausberger <pasci@pascals-MacBook-Pro.local>
2015-12-31 13:28:23 +0100

    change video

--- site/snippets/videohero.php
+++ site/snippets/videohero.php
@@ -23,12 +23,14 @@
 
     <div class="absolute left-0 top-0 full-height muted full-width bg-light-overlay-dark-gray z3"></div>
 
-    <div class="video-container sm-show">
-      <video class="" autoplay muted loop poster="<?= $site->url() ?>/video/bg.jpg" id="bgvid">
-        <source src="<?= $site->url() ?>/video/bg.mp4" type="video/mp4">
-        <source src="<?= $site->url() ?>/video/bg.ogv" type="video/ogg">
-      </video>
-    </div>
+    <?php if ($page->videoheader() == "videoYES") { ?>
+        <div class="video-container sm-show">
+          <video class="" autoplay muted loop poster="<?= $site->url() ?>/video/bg.jpg" id="bgvid">
+            <source src="<?= $site->url() ?>/video/bg.mp4" type="video/mp4">
+            <source src="<?= $site->url() ?>/video/bg.ogv" type="video/ogg">
+          </video>
+        </div>
+    <?php } ?>
 
   </div>
 
