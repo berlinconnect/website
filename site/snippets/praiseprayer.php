f91a48ee9af30f0bcd0147b160de744e820bfe01
Timothy ッ <timothy@achumba.com>
2016-02-12 10:04:54 +0100

    Initial praise and prayer report

---
+++ site/snippets/praiseprayer.php
@@ -0,0 +1,14 @@
+<div class="font-size-0">
+  <a href="" class="relative col-inline col-6 bc-white center py2 center-bg cover-bg" <?php if($image = $page->image('service.jpg')): ?>style="background-image: url(<?= $image->url() ?>)"<?php endif ?>>
+    <div class="py4 relative z1">
+      <h2 class="lighter mt0 mb0 tiny-mb0">Prayer Requests</h2>
+      <p class="h3 lighter px1 sm-px2 col-inline col-12 md-col-8"></p>
+    </div>
+  </a>
+  <a href="" class="relative col-inline col-6 bc-white center py2 center-bg cover-bg" <?php if($image = $page->image('service.jpg')): ?>style="background-image: url(<?= $image->url() ?>)"<?php endif ?>>
+    <div class="py4 relative z1">
+      <h2 class="lighter mt0 mb0 tiny-mb0">Praise Report</h2>
+      <p class="h3 lighter px1 sm-px2 col-inline col-12 md-col-8"></p>
+    </div>
+  </a>
+</div>
