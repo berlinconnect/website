7e083bd9545203f2d85b83620734f4987e67b5d1
Timothy ッ <timothy@achumba.com>
2016-03-06 08:37:36 +0100

    Removed hard coded text

--- site/snippets/praiseprayer.php
+++ site/snippets/praiseprayer.php
@@ -1,12 +1,12 @@
 <div class="font-size-0">
   <a href="<?= $page->prayerurl() ?>" class="praiseprayer relative col-inline col-12 sm-col-6 bc-white center py2 center-bg cover-bg border-box" <?php if($image = $page->image('prayer.jpg')): ?>style="background-image: url(<?= $image->url() ?>)"<?php endif ?>>
     <div class="py4 relative z1">
-      <h2 class="lighter mt0 mb0 tiny-mb0 line-height-0"><?= $page->prayertitle() ?>Prayer Requests</h2>
+      <h2 class="lighter mt0 mb0 tiny-mb0 line-height-0"><?= $page->prayertitle() ?></h2>
     </div>
   </a>
   <a href="<?= $page->praiseurl() ?>" class="praiseprayer relative col-inline col-12 sm-col-6 bc-white center py2 center-bg cover-bg border-box" <?php if($image = $page->image('praise.jpg')): ?>style="background-image: url(<?= $image->url() ?>)"<?php endif ?>>
     <div class="py4 relative z1">
-      <h2 class="lighter mt0 mb0 tiny-mb0 line-height-0"><?= $page->praisetitle() ?>Praise Report</h2>
+      <h2 class="lighter mt0 mb0 tiny-mb0 line-height-0"><?= $page->praisetitle() ?></h2>
     </div>
   </a>
 </div>
